56385f5f802e27a66206db89dcc5b6e307f2c513
vedha04 <srinivasvedha5@gmail.com>
2022-01-29 18:33:03 +0530

    commit after check

--- CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/entity/Admin.java
+++ CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/entity/Admin.java
@@ -9,6 +9,39 @@ package com.gqt.crs.entity;
  * 
  */
 public class Admin {
+	/**
+	 * @param admin_id
+	 * @param admin_pwd
+	 */
+	public Admin(int admin_id, String admin_pwd) {
+		super();
+		this.admin_id = admin_id;
+		this.admin_pwd = admin_pwd;
+	}
 	private int admin_id;
 	private String admin_pwd;
+	/**
+	 * @return the admin_id
+	 */
+	public int getAdmin_id() {
+		return admin_id;
+	}
+	/**
+	 * @param admin_id the admin_id to set
+	 */
+	public void setAdmin_id(int admin_id) {
+		this.admin_id = admin_id;
+	}
+	/**
+	 * @return the admin_pwd
+	 */
+	public String getAdmin_pwd() {
+		return admin_pwd;
+	}
+	/**
+	 * @param admin_pwd the admin_pwd to set
+	 */
+	public void setAdmin_pwd(String admin_pwd) {
+		this.admin_pwd = admin_pwd;
+	}
 }
--- CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/entity/Course.java
+++ CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/entity/Course.java
@@ -9,5 +9,9 @@ package com.gqt.crs.entity;
  * 
  */
 public class Course {
-
+	
+	private int course_id;
+	private String professor_name;
+	private String professor_email;
+	private String professor_pwd;
 }
