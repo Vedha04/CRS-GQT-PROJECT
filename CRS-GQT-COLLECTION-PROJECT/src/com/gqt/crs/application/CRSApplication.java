4a97ec1d461abfa8021e3a0cb3255353584248b0
vedha04 <srinivasvedha5@gmail.com>
2022-01-29 22:09:46 +0530

    multiple packages addition

---
+++ CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/application/CRSApplication.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.gqt.crs.application;
+
+/**
+ * @author vedha
+ *
+ * 
+ */
+public class CRSApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/constants/ConstantsImpl.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.gqt.crs.constants;
+
+/**
+ * @author vedha
+ *
+ * 
+ */
+public class ConstantsImpl {
+
+}
